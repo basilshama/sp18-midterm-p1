9c73498526aef653e7f0d0025b251b272af3cd44
Basil Abushama <basil@calvisitor-10-105-179-231.calvisitor.1918.berkeley.edu>
2018-03-07 15:29:46 -0800

    first changes to queue.sol

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -11,38 +11,51 @@ contract Queue {
 	/* State variables */
 	uint8 size = 5;
 	// YOUR CODE HERE
+	address[] _queue;
+	uint _numBuyers;
+
 
 	/* Add events */
 	// YOUR CODE HERE
 
 	/* Add constructor */
 	// YOUR CODE HERE
+	function Queue() public {
+		_queue = new address[](size);
+		_numBuyers = 0;
+	}
 
 	/* Returns the number of people waiting in line */
 	function qsize() constant returns(uint8) {
-		// YOUR CODE HERE
+		return _numBuyers;
 	}
 
 	/* Returns whether the queue is empty or not */
 	function empty() constant returns(bool) {
 		// YOUR CODE HERE
+		return _numBuyers == 0;
 	}
 	
 	/* Returns the address of the person in the front of the queue */
 	function getFirst() constant returns(address) {
 		// YOUR CODE HERE
+		return _queue[0];
 	}
 	
 	/* Allows `msg.sender` to check their position in the queue */
 	function checkPlace() constant returns(uint8) {
-		// YOUR CODE HERE
+		for (uint i = 0; i < _numBuyers; i++) {
+			if (msg.sender == _queue[i]) {
+				return i + 1;
+			}
+		}
 	}
 	
 	/* Allows anyone to expel the first person in line if their time
 	 * limit is up
 	 */
 	function checkTime() {
-		// YOUR CODE HERE
+		if
 	}
 	
 	/* Removes the first person in line; either when their time is up or when
@@ -50,10 +63,17 @@ contract Queue {
 	 */
 	function dequeue() {
 		// YOUR CODE HERE
+
 	}
 
 	/* Places `addr` in the first empty position in the queue */
 	function enqueue(address addr) {
 		// YOUR CODE HERE
+		for (uint i = 0; i < _queue; i++) {
+			if (_queue[i] == 0) {
+				_queue[i] == addr;
+				return;
+			}
+		}
 	}
 }
